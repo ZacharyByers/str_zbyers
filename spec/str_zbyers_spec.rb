db2229e1ad018fc1b20c28283ddfd7f4d5642147
Zach Byers <byerszach@gmail.com>
2018-01-16 10:20:48 -0700

    add something useful

--- spec/str_zbyers_spec.rb
+++ spec/str_zbyers_spec.rb
@@ -1,9 +1,19 @@
 RSpec.describe StrZbyers do
+  subject { StrZbyers }
+
   it "has a version number" do
     expect(StrZbyers::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do
+      expect(subject.spacify(str, 1)).to eq('M y   S t r i n g')
+    end
   end
 end
